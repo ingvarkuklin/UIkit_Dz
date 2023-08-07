ea98002332f2782781c90af2125ea0006f2f54c1
Igor Kuklin <igor_kuklin@inbox.ru>
2023-08-08 00:07:06 +0300

    init

---
+++ UIKit_Dz/UIKit_Dz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_Dz
+//
+//  Created by Игорь Куклин on 08.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
